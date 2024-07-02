a22041397985433196852b370c21b6c334937d04
Vasil Lozanov <vasillozanov1@gmail.com>
2024-07-02 20:36:24 +0300

    initial version

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/lozanov95/stock-checker
+
+go 1.22.0
+
+require github.com/PuerkitoBio/goquery v1.9.2
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/aws/aws-lambda-go v1.47.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+)
---
+++ ozone.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type OzoneChecker struct {
+	Replacer *strings.Replacer
+}
+
+func (oc *OzoneChecker) isItemAvailable(doc *goquery.Document) bool {
+	return doc.Find("#availability-holder.availability.out-of-stock").Length() == 0
+}
+
+func (oc *OzoneChecker) getPrice(doc *goquery.Document) float64 {
+	priceNode := doc.Find(".product-options .price")
+	if priceNode.Length() == 0 {
+		return 0
+	}
+
+	num, err := strconv.ParseFloat(
+		strings.TrimSpace(
+			oc.Replacer.Replace(
+				priceNode.Text(),
+			),
+		), 64)
+
+	if err != nil {
+		slog.Error(err.Error())
+		return 0
+	}
+
+	return num
+}
+
+func (oc *OzoneChecker) Check(url string) (CheckResponse, error) {
+	response := CheckResponse{}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return response, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return response, err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return response, err
+	}
+
+	response.Available = oc.isItemAvailable(doc)
+	response.Price = oc.getPrice(doc)
+
+	return response, nil
+}
+
+func NewOzoneChecker() *OzoneChecker {
+	return &OzoneChecker{
+		Replacer: strings.NewReplacer(
+			",", ".",
+			"лв.", "",
+			" ", ""),
+	}
+}
